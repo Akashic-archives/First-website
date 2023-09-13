34c508eee29efe19d75953b4d1dec21e26cf2c93
momo <mhamedbattioui@gmail.com>
2023-09-13 16:06:23 -0400

    new files and ...

--- app.py
+++ app.py
@@ -6,6 +6,30 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/horaire")
+def horaire():
+    return render_template("horaire.html")
+
+@app.route("/tos")
+def tos():
+    return render_template("tos.html")
+
+#@app.route("/forensics")
+#def home():
+#    return render_template("horaire.html")
+
+#@app.route("/cv")
+#def home():
+#    return render_template("cv.html")
+
+#@app.route("/index")
+#def home():
+#    return render_template("index.html")
+
+#@app.route("/moi")
+#def home():
+#    return render_template("moi.html")
+
 #@app.route("/<name>")
 #def user(name):
 #    return f"Hello {name}"
@@ -15,5 +39,5 @@ def home():
 #    return redirect(url_for("user", name="Admin"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5000) #port=xxxx
+    app.run(host="0.0.0.0", debug=True, port=80) #port=xxxx
 
